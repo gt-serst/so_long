9e965feb86e9d83378256500e8a07ae493689c79
Geraud T'Serstevens <gt-serst@mi-c2-p8.s19.be>
2023-06-07 12:28:13 +0200

    update parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:45:26 by gt-serst          #+#    #+#             */
-/*   Updated: 2023/06/07 11:13:44 by gt-serst         ###   ########.fr       */
+/*   Updated: 2023/06/07 12:09:12 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,14 +70,14 @@ static int	is_closed_by_walls(char **map)
 	int	col;
 
 	col = 0;
-	while (map[0][col] || map[count_rows(map) - 1][col])
+	while (map[0][col] && map[count_rows(map) - 1][col])
 	{
 		if (map[0][col] != 49 || map[count_rows(map) - 1][col] != 49)
 			return (0);
 		col++;
 	}
 	row = 0;
-	while (map[row] && (map[row][0] || map[row][ft_strlen(map[row]) - 1]))
+	while (map[row] && (map[row][0] && map[row][ft_strlen(map[row]) - 1]))
 	{
 		if (map[row][0] != 49 || map[row][ft_strlen(map[row]) - 1] != 49)
 			return (0);
@@ -100,12 +100,12 @@ void	parsing(t_program *program, char *argv)
 		exit(errno);
 	}
 	program->map = remove_nl(program->map);
-	if (!is_closed_by_walls(program->map))
-		exit_msg(program->map, "Map not closed by walls\n");
-	if (!is_rectangular(program->map))
-		exit_msg(program->map, "Map is not rectangular\n");
 	if (!check_type_of_components(program->map))
 		exit_msg(program->map, "Map contains wrong components\n");
+	if (!is_rectangular(program->map))
+		exit_msg(program->map, "Map is not rectangular\n");
+	if (!is_closed_by_walls(program->map))
+		exit_msg(program->map, "Map not closed by walls\n");
 	if (count_components(program->map, 'C') < 1
 		|| count_components(program->map, 'P') != 1
 		|| count_components(program->map, 'E') != 1)
